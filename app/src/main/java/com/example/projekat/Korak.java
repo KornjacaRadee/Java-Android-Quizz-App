1fe8bb6a85182a74c5cf5d89f410a488679c80ca
srdjan <srdjannn321@gmail.com>
2023-06-07 04:02:19 +0200

    korak po korak

--- app/src/main/java/com/example/projekat/Korak.java
+++ app/src/main/java/com/example/projekat/Korak.java
@@ -37,12 +37,22 @@ public class Korak extends AppCompatActivity {
     private int currentScore = 0;
     private final int SCORE_INCREMENT = 20;
 
+    private boolean textView21Opened = false;
+    private boolean textView23Opened = false;
+    private boolean textView24Opened = false;
+    private boolean textView25Opened = false;
+    private boolean textView26Opened = false;
+    private boolean textView27Opened = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.korak);
 
+
+
+
+        textView20 = findViewById(R.id.textView20);
         textView21 = findViewById(R.id.textView21);
         textView23 = findViewById(R.id.textView23);
         textView24 = findViewById(R.id.textView24);
@@ -55,10 +65,50 @@ public class Korak extends AppCompatActivity {
         textView21.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showTextAndStartTimer();
+                showTextAndStartTimer(textView21);
+                textView21Opened = true;
             }
         });
-        
+
+        textView23.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTextAndStartTimer(textView23);
+                textView23Opened = true;
+            }
+        });
+
+        textView24.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTextAndStartTimer(textView24);
+                textView24Opened = true;
+            }
+        });
+
+        textView25.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTextAndStartTimer(textView25);
+                textView25Opened = true;
+            }
+        });
+
+        textView26.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTextAndStartTimer(textView26);
+                textView26Opened = true;
+            }
+        });
+
+        textView27.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTextAndStartTimer(textView27);
+                textView27Opened = true;
+            }
+        });
 
         countDownTimer = new CountDownTimer(TIMER_DURATION, COUNTDOWN_INTERVAL) {
             @Override
@@ -84,66 +134,26 @@ public class Korak extends AppCompatActivity {
             }
         });
 
-
         Button sledecibtn = findViewById(R.id.sledecibtn);
-
+        sledecibtn.setEnabled(false);
         sledecibtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openActivityAcocijacije();
             }
         });
-
     }
-
-        /*Button button29 = findViewById(R.id.sledecibtn);
-        handler = new Handler();
-
-
-        *//*runnable = new Runnable() {
-            private int timeLeft = 30;
-            Button button29 = findViewById(R.id.sledecibtn);
-
-            @Override
-            public void run() {
-                timeLeft--;
-                textView20.setText(String.valueOf(timeLeft));
-
-                if (timeLeft <= 0) {
-                    // Vreme je isteklo
-                    Toast.makeText(getApplicationContext(), "Vreme je isteklo!", Toast.LENGTH_SHORT).show();
-
-                    sledecibtn.performClick();
-
-
-                } else {
-                    // Ponovo pokreće tajmer za svaku sekundu
-                    handler.postDelayed(this, 1000);
-                }
-            }
-        };*//*
-
-        handler.postDelayed(runnable, 1000);
-
-
-        textView20 = findViewById(R.id.textView20);*/
-
-
-
 
     public void openActivityAcocijacije() {
         Intent intent = new Intent(this, Asocijacije.class);
         startActivity(intent);
     }
 
-
-    private void showTextAndStartTimer() {
-        textView21.setVisibility(View.VISIBLE);
-        textView21.setText("Ovde se prikazuje određeni tekst");
+    private void showTextAndStartTimer(final TextView textView) {
+        textView.setVisibility(View.VISIBLE);
+        textView.setText("Ovde se prikazuje određeni tekst");
         countDownTimer.start();
     }
-
-
 
     private void showNotification() {
         Toast.makeText(this, "Unesite odgovor!", Toast.LENGTH_SHORT).show();
@@ -151,10 +161,36 @@ public class Korak extends AppCompatActivity {
 
     private void checkAnswer() {
         String userAnswer = answerEditText.getText().toString().trim();
+        int scoreIncrement = 20;
+
+        if (textView21Opened) {
+            scoreIncrement -= 0; // Oduzmi 2 boda ako je textView21 otvoren
+        }
+        if (textView23Opened) {
+            scoreIncrement -= 2; // Oduzmi 2 boda ako je textView23 otvoren
+        }
+        if (textView24Opened) {
+            scoreIncrement -= 2; // Oduzmi 2 boda ako je textView24 otvoren
+        }
+        if (textView25Opened) {
+            scoreIncrement -= 2; // Oduzmi 2 boda ako je textView25 otvoren
+        }
+        if (textView26Opened) {
+            scoreIncrement -= 2; // Oduzmi 2 boda ako je textView26 otvoren
+        }
+        if (textView27Opened) {
+            scoreIncrement -= 2; // Oduzmi 2 boda ako je textView27 otvoren
+        }
 
         if (userAnswer.equalsIgnoreCase("tacan_odgovor")) {
-            currentScore += SCORE_INCREMENT;
-            Toast.makeText(this, "Odgovor je tačan!", Toast.LENGTH_SHORT).show();
+            currentScore += scoreIncrement; // Dodaj ukupan broj bodova
+            Toast.makeText(this, "Odgovor je tačan! Dobili ste " + scoreIncrement + " bodova.", Toast.LENGTH_SHORT).show();
+
+            disableTextViews(); // Onemogući klikabilnost TextView elemenata
+            answerEditText.setEnabled(false); // Onemogući unos teksta
+
+            Button sledecibtn = findViewById(R.id.sledecibtn);
+            sledecibtn.setEnabled(true); // Omogući klikabilnost dugmeta "sledecibtn"
         } else {
             Toast.makeText(this, "Odgovor nije tačan!", Toast.LENGTH_SHORT).show();
         }
@@ -162,5 +198,17 @@ public class Korak extends AppCompatActivity {
         scoreTextView.setText(String.valueOf(currentScore));
         answerEditText.setText("");
     }
+
+    private void disableTextViews() {
+        textView21.setEnabled(false);
+        textView23.setEnabled(false);
+        textView24.setEnabled(false);
+        textView25.setEnabled(false);
+        textView26.setEnabled(false);
+        textView27.setEnabled(false);
     }
+}
+
+
+
 
