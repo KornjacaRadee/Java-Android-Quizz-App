80d5ee3e0093ceafe4cc2142f01681e671199310
srdjan <srdjannn321@gmail.com>
2023-06-07 18:35:32 +0200

    skocko

--- app/src/main/java/com/example/projekat/Skocko.java
+++ app/src/main/java/com/example/projekat/Skocko.java
@@ -2,6 +2,7 @@ package com.example.projekat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.EditText;
 
 public class Skocko extends AppCompatActivity {
 
@@ -21,6 +23,127 @@ public class Skocko extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.skocko);
+
+        Button buttonSkocko = findViewById(R.id.skocko);
+        Button buttonSrce = findViewById(R.id.srce);
+        Button buttonList = findViewById(R.id.list);
+        Button buttonKocka = findViewById(R.id.kocka);
+        Button buttonTref = findViewById(R.id.tref);
+        Button buttonPik = findViewById(R.id.pik);
+
+
+
+
+
+        EditText editText = findViewById(R.id.editTextText15);
+        EditText editText1 = findViewById(R.id.editTextText13);
+        EditText editText2 = findViewById(R.id.editTextText11);
+        EditText editText3 = findViewById(R.id.editTextText10);
+        EditText editText4 = findViewById(R.id.editTextText12);
+        EditText editText5 = findViewById(R.id.editTextText14);
+
+
+        View.OnClickListener buttonClickListener = new View.OnClickListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public void onClick(View v) {
+                String buttonValue = "";
+
+                if (v.getId() == R.id.srce) {
+                    buttonValue = buttonSrce.getText().toString();
+                } else if (v.getId() == R.id.skocko) {
+                    buttonValue = buttonSkocko.getText().toString();
+                } else if (v.getId() == R.id.list) {
+                    buttonValue = buttonList.getText().toString();
+                } else if (v.getId() == R.id.kocka) {
+                    buttonValue = buttonKocka.getText().toString();
+                } else if (v.getId() == R.id.tref) {
+                    buttonValue = buttonTref.getText().toString();
+                } else if (v.getId() == R.id.pik) {
+                    buttonValue = buttonPik.getText().toString();
+                }
+
+                String currentText = editText.getText().toString();
+                String newText = currentText + buttonValue;
+                editText.setText(newText);
+            }
+
+        };
+
+
+        buttonSrce.setOnClickListener(buttonClickListener);
+        buttonSkocko.setOnClickListener(buttonClickListener);
+        buttonList.setOnClickListener(buttonClickListener);
+        buttonKocka.setOnClickListener(buttonClickListener);
+        buttonTref.setOnClickListener(buttonClickListener);
+        buttonPik.setOnClickListener(buttonClickListener);
+
+
+
+        Button button28 = findViewById(R.id.button28);
+        TextView textView32 = findViewById(R.id.textView32);
+
+
+        button28.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tačnaKombinacija = "1234"; // Promenite vrednost na željenu tačnu kombinaciju
+                String unetaKombinacija = editText.getText().toString();
+                int brojTačnihMesta = 0;
+                int brojNetačnihMesta = 0;
+
+                // Provera za svaki znak u kombinaciji
+                for (int i = 0; i < unetaKombinacija.length(); i++) {
+                    if (i < tačnaKombinacija.length()) {
+                        char unetiKarakter = unetaKombinacija.charAt(i);
+                        char tačanKarakter = tačnaKombinacija.charAt(i);
+
+                        // Ako je znak na tačnom mestu
+                        if (unetiKarakter == tačanKarakter) {
+                            brojTačnihMesta++;
+                        } else if (tačnaKombinacija.indexOf(unetiKarakter) != -1) {
+                            // Ako znak postoji u tačnoj kombinaciji, ali nije na tačnom mestu
+                            brojNetačnihMesta++;
+                        }
+                    } else {
+                        // Ako je kombinacija duža od tačne kombinacije
+                        brojNetačnihMesta++;
+                    }
+                }
+
+                String rezultat = "Tačno mesta: " + brojTačnihMesta + ", Netačno mesta: " + brojNetačnihMesta;
+                textView32.setText(rezultat);
+
+                // Provera da li je uneta tačna kombinacija
+                if (unetaKombinacija.equals(tačnaKombinacija)) {
+                    // Unesena je tačna kombinacija
+
+                    int brojPokušaja = 6 - (Integer.parseInt(textView20.getText().toString()) - 1);
+                    int bodovi = 0;
+
+                    if (brojPokušaja <= 2) {
+                        bodovi = 20;
+                    } else if (brojPokušaja > 2 || brojPokušaja <4) {
+                        bodovi = 15;
+                    } else if (brojPokušaja > 4 || brojPokušaja <6) {
+                        bodovi = 10;
+                    }
+
+                    Toast.makeText(getApplicationContext(), "Rešenje je tačno! Osvojili ste " + bodovi + " bodova.", Toast.LENGTH_SHORT).show();
+
+                    // Povećaj broj bodova
+                    EditText editTextBodovi = findViewById(R.id.editTextText7);
+                    int trenutniBrojBodova = Integer.parseInt(editTextBodovi.getText().toString());
+                    int noviBrojBodova = trenutniBrojBodova + bodovi;
+                    editTextBodovi.setText(String.valueOf(noviBrojBodova));
+                } else if (Integer.parseInt(textView20.getText().toString()) == 1) {
+                    // Niste uspeli da pogodite resenje u prvih 6 puta
+                    Toast.makeText(getApplicationContext(), "Niste uspeli da pogodite resenje u prvih 6 puta.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
 
 
         Button sledecibtn = findViewById(R.id.button32);
